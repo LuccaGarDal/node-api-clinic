e644eea3a9c82955e4f5dc6aedd81e47e835a9f3
LuccaGarDal <luccagarciarosa@gmail.com>
2026-03-16 12:12:56 -0300

    fix: PORT uppercase

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 }); 
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
